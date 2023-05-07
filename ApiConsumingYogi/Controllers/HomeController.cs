bd810d0ef6961a61ec16824c78487217e6597ceb
MaryamTalebiMoghaddam <122522843+MaryamTalebiMoghaddam@users.noreply.github.com>
2023-05-07 12:26:15 +0430

    Add project files.

---
+++ ApiConsumingYogi/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using ApiConsumingYogi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ApiConsumingYogi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Reservation> reservationList = new List<Reservation>();
+            using (HttpClient httpClient =new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44324/api/Reservation"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+
+        public ViewResult GetReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetReservation(int id)
+        {
+            Reservation reservation = new();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44324/api/Reservation/" + id))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                    else
+                        ViewBag.StatusCode = response.StatusCode;
+                }
+            }
+            return View(reservation);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ApiConsumingYogi/Models/Reservation.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiConsumingYogi.Models
+{
+    public class Reservation
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+
+    }
+}
